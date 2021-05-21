de2d81aaf5f7a56ecc31555d867e58a83ef2751f
Bhavesh Lokre <bhaveshlokre11@gmail.com>
2021-05-21 16:27:51 +0000

    first commit

---
+++ birthday-reminder/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
